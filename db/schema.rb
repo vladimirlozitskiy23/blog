1e36c175caa62ee9e3ad99e5b4e03dd22b361055
vladimirlozitskiy23 <rubyonrails2023@gmail.com>
2023-04-01 16:17:57 +0300

    Add form to contacts new

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_19_185613) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_31_185719) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "email"
+    t.text "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
