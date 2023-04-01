3b68204194e863d6eedc7a1928a00043c612e4ba
vladimirlozitskiy23 <rubyonrails2023@gmail.com>
2023-04-01 18:34:23 +0300

    Add contact validation

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,7 +6,11 @@ class ContactsController < ApplicationController
     def create
         # render plain: params[:contact].inspect
         @contact = Contact.new(contact_params)
-        @contact.save
+        if @contact.valid?
+            @contact.save
+        else
+            render action: 'new'
+        end
     end
 
     private
