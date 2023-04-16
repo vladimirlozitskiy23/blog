c716e27ce8f890d7a349dbfde328c3eefb2e9efc
vladimirlozitskiy23 <rubyonrails2023@gmail.com>
2023-04-16 12:32:05 +0300

    Add edit and update action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,13 +16,25 @@ class ArticlesController < ApplicationController
     def create
         # render plain: params[:article].inspect
         @article = Article.new (article_params)
-        if @article.valid?
-            @article.save
+        if  @article.save
             redirect_to @article
         else
             render action: 'new'
         end
         
+    end
+
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def update
+        @article = Article.find (params[:id])
+        if @article.update(article_params)
+            redirect_to @article
+        else
+            render action: 'edit'
+        end
     end
 
     private
