d12e895d2bbfec9852c874380d4910b18ab82f77
vladimirlozitskiy23 <rubyonrails2023@gmail.com>
2023-04-29 11:39:57 +0300

    Add comment to post

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   get 'terms' => 'pages#terms'
   get 'about' => 'pages#about'
   resource :contacts, only: [:new, :create], path_names: {:new => ''}
-  resources :articles 
+  resources :articles do
+    resources :comments 
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
