b2261d2f67f4de015c396d3e3e03dca0eafef200
vladimirlozitskiy23 <rubyonrails2023@gmail.com>
2023-08-11 20:33:22 +0300

    update article spec

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,12 @@
 require 'spec_helper'
 
 describe Article do
+  describe 'validation' do
+    it {should validate_presence_of :title}
+    it {should validate_presence_of :text}
+  end
+
+  describe 'association' do
     it {should have_many :comments}
+  end
 end
