70dcbb68f707546d0f57676f249e76b01e7e8099
vladimirlozitskiy23 <rubyonrails2023@gmail.com>
2023-09-16 21:06:31 +0300

    Add comments to methods

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -5,6 +5,8 @@ FactoryGirl.define do
 
     factory :article_with_comments do
       after :create do |article, evaluator|
+        #метод принимает название фабрики на основе которой будет создан массив
+        #сущностей,количество сущностей и перезаписываемые аттрибуты
         create_list :comment, 3, article: article
       end
     end
--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do
   factory :comment do
     author 'Mike'
+    #метод принимает свойство и считает сколько раз создается сущность
     sequence(:body) {|n| "comment body #{n}"}
   end
 end
