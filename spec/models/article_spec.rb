2ed771b6513d9df2845b98714ce1f8f24c668771
vladimirlozitskiy23 <rubyonrails2023@gmail.com>
2023-09-16 21:18:21 +0300

    Add length article spec

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -5,6 +5,7 @@ require 'spec_helper'
 describe Article do
   describe 'validation' do
     it {should validate_presence_of :title}
+    it {should validate_length_of(:title).is_at_most(140)}
     it {should validate_presence_of :text}
   end
 
