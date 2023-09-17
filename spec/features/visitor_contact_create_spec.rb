0c44946c61d74a553d0d89728b7c8cddb54570d3
vladimirlozitskiy23 <rubyonrails2023@gmail.com>
2023-09-17 16:53:18 +0300

    Add contact page spec

--- spec/features/visitor_contact_create_spec.rb
+++ spec/features/visitor_contact_create_spec.rb
@@ -3,6 +3,6 @@ require "spec_helper"
 feature 'Contact creation' do 
   scenario 'allow access contacts page' do 
     visit '/contacts'
-    expect(page).to have_content 'Contacts'
+    expect(page).to have_content I18n.t('contacts.contact_us')
   end
 end
