3fa91f15d52d398a1f4fc271fc197e82a69991b0
vladimirlozitskiy23 <rubyonrails2023@gmail.com>
2023-09-17 17:46:52 +0300

    Update tests(allows a guest create contact)

--- spec/features/visitor_contact_create_spec.rb
+++ spec/features/visitor_contact_create_spec.rb
@@ -5,4 +5,13 @@ feature 'Contact creation' do
     visit '/contacts'
     expect(page).to have_content I18n.t('contacts.contact_us')
   end
+
+  scenario 'allows a guest create contact' do 
+    visit '/contacts'
+    fill_in :contact_email, :with => 'user@example.com'
+    fill_in :contact_message, :with => 'something'
+    click_button 'Send message'
+
+    expect(page).to have_content 'Thanks!'
+  end
 end
